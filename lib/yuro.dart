95b80a6ebebbf03991888b9a19c442a92ed1aff2
dugang <timor.du@hotmail.com>
2021-07-13 15:06:31 +0800

    增加yuro_upgrade导出
    (cherry picked from commit 9c3752079ef9ef6eb1b76c9ac6588e7c8164ed5f)

--- lib/yuro.dart
+++ lib/yuro.dart
@@ -8,4 +8,5 @@ export 'package:yuro/yuro_extension/yuro_extension.dart';
 export 'package:yuro/yuro_state/yuro_state.dart';
 export 'package:yuro/yuro_route/yuro_route.dart';
 export 'package:yuro/yuro_translation/yuro_translation.dart';
+export 'package:yuro/yuro_upgrade/yuro_upgrade.dart';
 export 'package:yuro/yuro_widget/yuro_widget.dart';
