50b6110e00c33f27fcc0126bebf6ba201bc1220a
dugang <timor.du@hotmail.com>
2021-07-27 11:02:04 +0800

    修改Obs组件参数名称
    (cherry picked from commit 483e0e27ea510e2a174e24ab3c5749fdd6285820)

--- lib/yuro_state/src/obs/obs_value.dart
+++ lib/yuro_state/src/obs/obs_value.dart
@@ -2,18 +2,19 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class Obs<T> extends StatelessWidget {
-  const Obs({Key? key, required this.value, required this.builder, this.child}) : super(key: key);
+  const Obs({Key? key, required this.valueListenable, required this.builder, this.child}) : super(key: key);
 
-  final ValueNotifier<T> value;
+  final ValueNotifier<T> valueListenable;
 
   final Widget? child;
 
   final ValueWidgetBuilder<T> builder;
 
   @override
-  Widget build(BuildContext context) => ValueListenableBuilder<T>(valueListenable: value, builder: builder, child: child);
+  Widget build(BuildContext context) =>
+      ValueListenableBuilder<T>(valueListenable: valueListenable, builder: builder, child: child);
 }
 
-extension ObsExt<T> on T{
+extension ObsExt<T> on T {
   ValueNotifier<T> get obs => ValueNotifier<T>(this);
 }
