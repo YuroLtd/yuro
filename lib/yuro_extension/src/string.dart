b237ce88645107b212b13d91ecb23337039a83c8
dugang <timor.du@hotmail.com>
2021-08-13 10:31:44 +0800

    修改String 转double结果
    (cherry picked from commit 21332691308bb26569e7ae16b22f421c74776c14)

--- lib/yuro_extension/src/string.dart
+++ lib/yuro_extension/src/string.dart
@@ -3,7 +3,7 @@ extension StringExt on String {
 
   bool isDigit() => int.tryParse(this) != null;
 
-  double parseToDouble() => double.parse(this);
+  double? toDouble() => double.tryParse(this);
 
   /// 判断字符串是否是手机号
   bool isPhone() => RegExp(r'^1(3|4|5|6|7|8|9)\d{9}$').hasMatch(this);
