a86b5c4f23cd5ee6d5cfd768e5596f2da070dffd
dugang <timor.du@hotmail.com>
2021-09-10 17:50:00 +0800

    [yuro]网络请求生成器代码配套修改 [yuro_generator]网络请求生成器代码

---
+++ lib/yuro_connect/http.src/dio_client.dart
@@ -0,0 +1,112 @@
+import 'dart:io';
+
+import 'package:dio/adapter.dart';
+import 'package:dio/dio.dart';
+
+class DioClient {
+  final Dio _dio = Dio();
+
+  set baseUrl(String url) {
+    _dio.options.baseUrl = url;
+  }
+
+  set connectTimeout(Duration timeout) {
+    _dio.options.connectTimeout = timeout.inMilliseconds;
+  }
+
+  set receiveTimeout(Duration timeout) {
+    _dio.options.receiveTimeout = timeout.inMilliseconds;
+  }
+
+  set sendTimeout(Duration timeout) {
+    _dio.options.sendTimeout = timeout.inMilliseconds;
+  }
+
+  set contentType(String contentType) {
+    _dio.options.contentType = contentType;
+  }
+
+  set responseType(ResponseType type) {
+    _dio.options.responseType = type;
+  }
+
+  set headers(Map<String, dynamic> headers) {
+    _dio.options.headers.addAll(headers);
+  }
+
+  set queryParameters(Map<String, dynamic> parameters) {
+    _dio.options.queryParameters.addAll(parameters);
+  }
+
+  set extras(Map<String, dynamic> extras) {
+    _dio.options.extra.addAll(extras);
+  }
+
+  set validateStatus(ValidateStatus validateStatus) {
+    _dio.options.validateStatus = validateStatus;
+  }
+
+  set receiveDataWhenStatusError(bool receiveDataWhenStatusError) {
+    _dio.options.receiveDataWhenStatusError = receiveDataWhenStatusError;
+  }
+
+  set followRedirects(bool followRedirects) {
+    _dio.options.followRedirects = followRedirects;
+  }
+
+  set maxRedirects(int maxRedirects) {
+    _dio.options.maxRedirects = maxRedirects;
+  }
+
+  set requestEncoder(RequestEncoder requestEncoder) {
+    _dio.options.requestEncoder = requestEncoder;
+  }
+
+  set responseDecoder(ResponseDecoder responseDecoder) {
+    _dio.options.responseDecoder = responseDecoder;
+  }
+
+  /// eg. 192.168.0.1:8888
+  void openProxy(String proxy) {
+    (_dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (client) {
+      client.findProxy = (uri) => "PROXY $proxy";
+    };
+  }
+
+  void setCertificate(String pem) {
+    (_dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (client) {
+      client.badCertificateCallback = (cert, host, port) => cert.pem == pem;
+    };
+  }
+
+  void setCertificateFile(File file) {
+    (_dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (client) {
+      return HttpClient(context: SecurityContext()..setTrustedCertificates(file.path));
+    };
+  }
+
+  void addInterceptor(Interceptor interceptor) {
+    _dio.interceptors.add(interceptor);
+  }
+
+  void addInterceptors(List<Interceptor> interceptors) {
+    _dio.interceptors.addAll(interceptors);
+  }
+
+  void openLog({
+    bool request = false,
+    bool requestHeader = false,
+    bool requestBody = false,
+    bool responseHeader = false,
+    bool responseBody = false,
+  }) {
+    addInterceptor(LogInterceptor(
+        request: request,
+        requestHeader: requestHeader,
+        requestBody: requestBody,
+        responseHeader: responseHeader,
+        responseBody: responseBody));
+  }
+
+  Dio build() => _dio;
+}
--- lib/yuro_connect/yuro_connect.dart
+++ lib/yuro_connect/yuro_connect.dart
@@ -1,6 +1,10 @@
 library yuro_connect;
 
-export 'package:dio/dio.dart';
+export 'package:dio/dio.dart' hide Headers;
+
+export 'http.src/dio_client.dart';
+
+
 export 'http.src/http_base.dart';
 export 'http.src/http_base_mixin.dart';
 export 'http.src/util.dart';
