74be4a1a8a560937470627f44b8a12088e8b8bc8
dugang <timor.du@hotmail.com>
2022-12-13 10:23:36 +0800

    更新Isar依赖版本,增加IsarExt

--- lib/storage/src/isar/isar.dart
+++ lib/storage/src/isar/isar.dart
@@ -1,1 +1,21 @@
+import 'package:isar/isar.dart';
+
 export 'package:isar/isar.dart';
+
+extension IsarExt on Isar {
+  Future<T> writeIsar<T>(Future<T> Function(Isar isar) callback, {bool silent = false}) async {
+    return await writeTxn(() => callback.call(this), silent: false);
+  }
+
+  T writeIsarSync<T>(T Function(Isar isar) callback, {bool silent = false}) {
+    return writeTxnSync(() => callback.call(this), silent: false);
+  }
+
+  Future<T> readIsar<T>(Future<T> Function(Isar isar) callback) async {
+    return await txn(() => callback.call(this));
+  }
+
+  T readIsarSync<T>(T Function(Isar isar) callback) {
+    return txnSync(() => callback.call(this));
+  }
+}
