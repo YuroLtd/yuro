0ab354aa3179dfeb1b676133b9b4b6be4e5bcdd7
dugang <timor.du@hotmail.com>
2023-01-05 09:33:56 +0800

    优化EventBusExt

--- lib/util/src/event_bus.dart
+++ lib/util/src/event_bus.dart
@@ -6,10 +6,12 @@ import 'package:yuro/state/state.dart';
 import 'package:yuro/util/util.dart';
 
 extension EventBusExt on YuroInterface {
-  Stream<T> listenOn<T>() => Yuro.eventBus.stream.where((event) => event is T).cast<T>();
+  Stream<T> stream<T>() => Yuro.eventBus.stream.where((event) => event is T).cast<T>();
 
-  Stream<Event> listenOnEvent([List<int> codes = const []]) =>
-      listenOn<Event>().where((event) => codes.isNotEmpty ? codes.contains(event.code) : true);
+  Stream<Event> streamOn(int code1, [int? code2, int? code3, int? code4, int? code5, int? code6]) {
+    final codes = [code1, code2, code3, code4, code5, code6].whereNotNull();
+    return stream<Event>().where((event) => codes.contains(event.code));
+  }
 
   void fire(dynamic event) => Yuro.eventBus.add(event);
 }
