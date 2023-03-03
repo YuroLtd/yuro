e6d730383fc2bbd743f7fbb7f9148ab005bd83a8
dugang <timor.du@hotmail.com>
2023-03-03 17:23:59 +0800

    移除socket和mqtt,交由项目自己实现

--- lib/connect/connect.dart
+++ lib/connect/connect.dart
@@ -1,13 +1,6 @@
 export 'package:dio/dio.dart';
 
 // http
-export 'http.src/dio_ext.dart';
-export 'http.src/observer.dart';
-export 'http.src/service.dart';
-
-// mqtt
-export 'mqtt.src/service.dart';
-
-// socket
-export 'socket.src/client/client.dart';
-export 'socket.src/server/server.dart';
+export 'src/dio_ext.dart';
+export 'src/observer.dart';
+export 'src/service.dart';
