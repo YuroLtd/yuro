2f9ce8feb6e3368f139728f15b5735a1f33aeaa1
dugang <timor.du@hotmail.com>
2023-03-16 09:48:40 +0800

    优化依赖库版本

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -3,7 +3,6 @@ library util;
 export 'src/collection.dart';
 export 'src/file_system.dart';
 export 'src/map.dart';
-export 'src/focus.dart';
 export 'src/num.dart';
 export 'src/path_provider.dart';
 export 'src/regex.dart';
@@ -15,5 +14,7 @@ export 'src/date_time.dart';
 export 'src/crc.dart';
 export 'src/convert.dart';
 export 'src/worker.dart';
+export 'src/yuro.dart';
 
 export 'package:collection/collection.dart';
+export 'package:json_annotation/json_annotation.dart';
